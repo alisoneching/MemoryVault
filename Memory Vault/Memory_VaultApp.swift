21dbd70ac9a250c543366fd78296d347f5cdb3c0
Alison Ching <alisonemilyching@gmail.com>
2024-10-20 13:58:18 -0700

    Initial Commit

---
+++ Memory Vault/Memory_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_VaultApp.swift
+//  Memory Vault
+//
+//  Created by Alison Ching on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Memory_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
