cddc030133e5b2ce3eba5ed6e907a9023d5f90ba
Elsa Zheng <zhengelsa150@gmail.com>
2024-11-04 17:16:46 -0800

    Finished Add Capsule View
    Formatting is now fixed.
    Issues: Buttons were working two days ago, but now all buttons in the app except Login seem to be broken

--- AddCapsuleView.swift
+++ AddCapsuleView.swift
@@ -1,84 +1,152 @@
 import SwiftUI
+/*
+    Since we scaled down, I removed the friends section
+    Can comment back in if changed mind
+    - Elsa
+ */
+
 
 struct AddCapsuleView: View {
-    // Define colors
-    private let backgroundColor = Color(red: 186/255, green: 194/255, blue: 170/255)
-    private let boxColor = Color(red: 105/255, green: 113/255, blue: 90/255)
-    private let friendBoxColor = Color(red: 239/255, green: 245/255, blue: 239/255)
+    //Colors needed
+    let backgroundColor = Color(red: 186/255,green: 194/255, blue:170/255 )
+    let boxColor = Color(red: 105/255, green: 113/255, blue: 90/255)
+    let buttonColor = Color(red: 78/255, green: 84/255, blue: 67/255)
+    /*
+    let friendBoxColor = Color(red: 239/255, green: 245/255, blue: 239/255)
     
-    // Dummy list of friends
-    private let friendList = ["Alice", "Bob", "Charlie"]
+    //fake stand in list of friends
+    let friendList = ["asjfnsk", "bsdnskj", "cskdcnsk"]
+    */
     
-    // State properties for user input
+    //For the date
+    @State private var unlockTime = Date()
+    @State private var lockTime = Date()
+    
+    //For storing capsule name
     @State private var capsuleName: String = ""
-    @State private var lockTime: Date = Date()
-    @State private var unlockTime: Date = Date()
-    
     var body: some View {
-        ZStack {
-            // Background color for entire view
+        ZStack{
+            //set background color
             backgroundColor.edgesIgnoringSafeArea(.all)
-            
-            VStack(alignment: .center, spacing: 20) {
-                // Title
+            VStack(alignment: .center) {
                 Text("Create New Capsule")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(boxColor)
-                
-                // Capsule Name TextField
+                    .font(.title) //font type
+                    .foregroundColor(.white) //font color
+                    .frame(width: 775, height: 50) //make frame around text
+                    //Color of the frame containing "Create New Capsule"
+                    .background(Rectangle().fill(boxColor))
+                    .border(boxColor)
+                    
+                //Set Capsule Name
                 TextField("Capsule Name", text: $capsuleName)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-                
-                // Friends list (scrollable)
-                ScrollView {
-                    VStack(alignment: .center, spacing: 10) {
-                        ForEach(friendList, id: \.self) { friend in
-                            Text(friend)
-                                .padding()
-                                .background(friendBoxColor)
-                                .cornerRadius(10)
-                                .frame(maxWidth: .infinity)
-                        }
+                    //.padding()
+                    .foregroundColor(Color.gray) //text color
+                    .background(Color.gray.opacity(0.2)) //background of box thing color
+                    .border(boxColor) //background of box thing color
+                    .cornerRadius(30) //shape of box
+                    .frame(width: 250, height: 100) //size of box
+                    
+                /*
+                //Scroll through friends
+                //Add friends later
+                ScrollView{
+                    VStack(alignment: .center, spacing: 10){
+                        ForEach(friendList, id: \.self){ friend in
+                                Text( friend)
+                                    .padding()
+                                    .foregroundColor(Color.black) //text color
+                                    .background(friendBoxColor) //box background color
+                                    .cornerRadius(1) //box shape
+                                    .border(boxColor) //border color
+                            }//end of friends list
+                        }//end of VStack for friends list
+                    }//end of ScrollView for friends list
+                    //put button into scroll view later
+                HStack{
+                    //Button to add friends
+                    Button("Invite"){
+                        //Insert logic here later
                     }
-                    .padding(.horizontal)
-                }
-                
-                // Lock time picker
+                }//end of HStack for invite button
+                */
+                    
+                //Setting lock time
                 Text("Set lock time:")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                    .font(.title) //font type
+                    .foregroundColor(.white) //font color
+                    .font(.system(size: 20)) //font size
+                //Date thing
                 DatePicker("", selection: $lockTime, displayedComponents: [.date, .hourAndMinute])
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                    .padding(.horizontal)
-                
-                // Unlock time picker
+                    .frame(width: 175, height: 50)
+                    
+                //Setting unlock time
                 Text("Set unlock time:")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                    .font(.title) //font type
+                    .foregroundColor(.white) //font color
+                    .font(.system(size: 20)) //font size
+                //Date thing
                 DatePicker("", selection: $unlockTime, displayedComponents: [.date, .hourAndMinute])
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                    .padding(.horizontal)
-                
-                // Create button
+                    .frame(width: 175, height: 50)
+                    
+                //Create button that takes you to the page adding stuff to the capsule
                 NavigationLink(destination: MyNewCapsuleView()) {
                     Text("Create")
                         .font(.headline)
                         .padding()
-                        .frame(maxWidth: .infinity)
                         .background(Color.blue)
                         .foregroundColor(.white)
                         .cornerRadius(10)
-                }
-                .padding(.horizontal)
+                }//end of NavigationLink to MyNewCapsule Page
+                .padding()
+                .padding()
+                    
+                //Icons
                 
-                Spacer()
-            }
-        }
-    }
-}
+                HStack{
+                    //Button to send user to something Page
+                    NavigationLink(destination: NotificationsView()){
+                        Image("today")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 45)
+                    }//end of NavigationLink for
+                    
+                    //Button to send user to Notifications Page
+                    NavigationLink(destination: NotificationsView()){
+                        Image("Icon")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 45)
+                    }//end of NavigationLink for Notifications Page button
+                    
+                    //Just an image for add capsule
+                    Image("add")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 45)
+                    
+                    //Button to send user to Friends Page
+                    NavigationLink(destination: FriendsView()){
+                        Image("favorite")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 45)
+                    }//end of NavigationLink for Friends page button
+                        
+                        //Button to send user to Profile Page
+                    NavigationLink(destination: ProfileView()){
+                        Image("account_circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 45)
+                    }//end of NavigationLink for Profile Page button
+                }//end of HStack for icons
+                .frame(width: 775, height: 75) //make frame around icons
+                //Color of the frame containing the icons
+                .background(Rectangle().fill(boxColor))
+                .border(boxColor)
+            }//end of VStack
+        }//end of ZStack
+    }//end of body
+}//end of AddCapsuleView
+
--- Memory Vault/Memory_VaultApp.swift
+++ Memory Vault/Memory_VaultApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Memory_VaultApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           ContentView()
         }
     }
 } 
